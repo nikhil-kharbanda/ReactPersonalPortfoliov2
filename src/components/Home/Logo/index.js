798d905373baadd1c78cd2c3672d47a98ff0022c
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-06-28 09:05:53 -0400

    Animated logo works!

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useLayoutEffect, useRef } from 'react';
 import gsap from 'gsap-trial';
 import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
 import LogoS from '../../../assets/images/logo-s.png';
@@ -10,34 +10,36 @@ const Logo = () => {
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
+  useLayoutEffect(() => {
 
+    let ctx = gsap.context(() => {
+      gsap.registerPlugin(DrawSVGPlugin)
 
+      gsap
+        .timeline()
+        .to(bgRef.current, {
+          duration:1,
+          opacity: 1,
+        })
+        .from(outlineLogoRef.current, {
+          drawSVG: 0,
+          duration: 20,
+        })
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration:1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    )
-  }, [])
+      gsap.fromTo(
+        solidLogoRef.current,
+        {
+          opacity: 0,
+        },
+        {
+          opacity: 1,
+          delay: 4,
+          duration: 4,
+        }
+      )
+      });
+      return () => ctx.revert();
+    }, []);
 
   return (
     <div className="logo-container" ref={bgRef}>
