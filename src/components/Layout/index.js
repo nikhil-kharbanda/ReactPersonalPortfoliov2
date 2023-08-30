34ddd2cbdf6b2a83ffbcf729d4337b225bd73143
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-08-29 21:12:21 -0400

    html tag shows on default page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,21 +3,26 @@ import Sidebar from '../Sidebar'
 import './index.scss';
 
 const Layout = () => {
-    return  (
-    <div className='App'>
-        <Sidebar />
-        <div className='page'>
-            <span className="tags top-tags">&lt;body&gt;</span>
+    return (
+        <div className='App'>
+            <Sidebar />
+            <div className='page'>
 
-            <Outlet />
+            <span className='tags top-tags'>&lt;html&gt;
+                    <br />
+                    <span className='top-tag-html'>&lt;body&gt;</span>
+                </span>
 
-            <span className = 'tags bottom-tags'>&lt;body&gt;
-            <br />
-            <span className = 'bottom-tag-html'>&lt;html&gt;</span>
-            </span>
 
+                <Outlet />
+
+                <span className='tags bottom-tags'>&lt;/body&gt;
+                    <br />
+                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
+                </span>
+
+            </div>
         </div>
-    </div>
     )
 }
 
