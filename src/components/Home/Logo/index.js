58a77853ec76db39a4d8c9be92b200f35d9e4654
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-08-31 16:15:05 -0400

    Removed bitmoji for deploy

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import './index.scss';
 import { motion } from 'framer-motion'
-import Me from '../../../assets/images/Bitmoji.png'
+// import Me from '../../../assets/images/Bitmoji.png'
 
 
 
@@ -9,10 +9,10 @@ const Logo = () => {
     <>
       <motion.div className='logo-container'>
         <motion.div>
-          <motion.img 
+          {/* <motion.img 
           className="avatar" src={Me} alt="ProfilePic" initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
-          transition={{ type: "spring", delay: 1, duration: 2, bounce: 0.75 }}/>
+          transition={{ type: "spring", delay: 1, duration: 2, bounce: 0.75 }}/> */}
         </motion.div>
       </motion.div>
 
