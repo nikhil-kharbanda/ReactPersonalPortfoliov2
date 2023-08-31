2bfc9bd18bfb49baee8c76d7878beb5f272bb4b0
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-08-30 21:33:34 -0400

    Removed BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About';
