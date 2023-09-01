a680a8e18de1265c1eda220a8b085a44fe2a74e4
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-09-01 15:06:58 -0400

    Contact container name adjusted

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -31,7 +31,7 @@ const Contact = () => {
 
     return (
          <>
-            <div className='container contact-page'>
+            <div className='container-contact contact-page'>
                 <div className='text-zone'>
                     <h1>
                         <AnimatedLetters 
