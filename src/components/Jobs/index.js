3042801c7b6f68d506082ee74a7e16b1da80e6df
Nikhil Kharbanda <nikhilkharbanda@cmail.carleton.ca>
2023-09-01 23:34:30 -0400

    Prelim for jobs pages

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,61 @@
+import { useEffect, useState, useRef } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
+import './index.scss'
+import { EngProjData } from '../data/EngProjData'
+import EngProjCard from '../subComponents/EngProjCard'
+import { motion } from 'framer-motion'
+
+const Education = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    let timeout
+    timeout = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => {
+      clearTimeout(timeout)
+    }
+  }, [])
+
+  const [height, setHeight] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setHeight(carousel.current.scrollHeight - carousel.current.offsetHeight)
+  }, []);
+
+  return (
+    <>
+      <div className="container-engproj engproj-page">
+        <div className="text-zone engproject-title" >
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={[
+                'E','n','g','i','n','e','e','r','i','n','g', ' ','P','r','o','j','e','c','t','s']}
+              idx={10}
+            />
+          </h1>
+          </div>
+          {/* TODO: Add education cards */}
+          <div className='box-engproj'>
+            <div ref={carousel} className='center-engproj' whileTap={{ cursor: "grabbing" }} style={{ originX: 0.5 }}>
+              <motion.div dragConstraints={{top: 10, bottom: -height - 150}} className='gridEngProjs'>
+                {EngProjData.map((Proj) => {
+                  return <EngProjCard key={Proj.id} proj={Proj} />
+                })}
+              </motion.div>
+          </div>
+          {/* END TODO: Add education cards */}
+
+        </div>
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Education
---
+++ src/components/data/PrevJobsData.js
@@ -0,0 +1,15 @@
+export const PrevJobsData = [
+  {
+    id: 1,
+    name: 'MirrAI',
+    description: ['For my final year engineering Capstone project, my group and I decided to create a smart home Magic Mirror with an AI component. This AI was designed to recognize what the user is currently wearing, then generates similar outfits based on that. The goal of this mirror was to help inspire users with similar outfits, giving them a variation on their everyday apparel. This project also allows users an ease of getting ready for the day, whether it being meetings, social gatherings or just going for a walk'],
+    imgSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8BPvDPBVVo44yCp1WBUofzp4JA1WVrcIx5Q&usqp=CAU',
+  },
+
+  {
+    id: 2,
+    name: 'RC Cars with proximity sensing ',
+    description: ['I had build a RC car using an old Roomba that had proximity sensing capabilities running on a RPi and ultrasonic sensors. The car would autonomously drive down a "road", where another RC car would be coming head on. In this scenario, the car running the RPi would sense the car coming closer, and would swerve to avoid it. Our next goal to take this further was to create an autonomous arm for which the car would automatically drive to an available "parking recharge spot" once a low battery was detected and the arm would recharge the on-board battery." '],
+    imgSrc: 'https://www.labmanautomation.com/wp-content/uploads/2013/07/vyuhju.jpg',
+  },
+]
