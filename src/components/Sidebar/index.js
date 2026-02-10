9ab5e1adcb6cce186999de217e46d59728b1ef18
Nikhil <nikhil.kharbanda@hotmail.com>
2026-02-10 10:52:27 -0500

    Missed remove unused import

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,7 +6,7 @@ import LogoK from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBriefcase, faEnvelope, faHome, faSchool, faUser, faWrench } from '@fortawesome/free-solid-svg-icons'
-import { faDev, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 
 const Sidebar = () => (
